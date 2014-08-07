fedb04c476895338d9020fe48437ab44996e7553
Edson Kenji Ninomiya <kenjinino07@gmail.com>
2014-08-07 13:11:04 -0300

    Fixing xdg-open problem when using vagrant

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
@@ -208,7 +208,6 @@ module.exports = function (grunt) {
     'recess',
     'concurrent:server',
     'connect:livereload',
-    'open',
     'watch'
   ]);
 
