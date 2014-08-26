6cc48b4ec0918240872bb0d314437309b80130a9
Edson Kenji Ninomiya <kenjinino07@gmail.com>
2014-08-26 16:16:16 -0300

    Bug fix: clicking on the clock svg generated an undefined error

--- app/scripts/datePicker.js
+++ app/scripts/datePicker.js
@@ -752,7 +752,8 @@ Module.directive('datePicker', ['datePickerConfig', '$filter', '$locale', '$docu
           var isNodeChild;
           isNodeChild = function(node) {
             while (node !== null) {
-              if (node === element[0] || (node.className !== undefined && node.className.indexOf('datepicker-calendar-day') >= 0)) {
+              debugger
+              if (node === element[0] || (node.className !== undefined && node.classList.contains('datepicker-calendar-day'))) {
                 scope.isDatepickerVisible = true;
                 return true;
               }
@@ -775,7 +776,7 @@ Module.directive('datePicker', ['datePickerConfig', '$filter', '$locale', '$docu
 
       var bindDblclick = function() {
         return $document.bind('dblclick', function(event) {
-          if(event.target.className.indexOf('datepicker-calendar-day') >= 0) {
+          if(event.target.className !== undefined && event.target.classList.contains('datepicker-calendar-day') >= 0) {
             scope.isDatepickerVisible = false;
             close();
           }
